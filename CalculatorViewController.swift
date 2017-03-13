bd9ae47bd54a792a0a05b1c27a610257c511fb64
BryanBonner <bonnerbryan@gmail.com>
2017-03-13 11:48:23 -0700

    initial

---
+++ CalculatorViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiscountCalculator
+//
+//  Created by CampusUser on 3/13/17.
+//  Copyright © 2017 CPSC411. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
